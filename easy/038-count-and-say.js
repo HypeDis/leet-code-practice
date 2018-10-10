5c2338c1384ee99e240e24ecc55ec7332e58f644
HypeDis-2 <30463842+HypeDis@users.noreply.github.com>
2018-10-10 00:13:02 -0700

    added comments to problem 38

--- easy/038-count-and-say.js
+++ easy/038-count-and-say.js
@@ -18,32 +18,40 @@
 var countAndSay = function(n) {
   // iterate through a string 
   let say = '1'
+  // base case for when n = 1
   if (n === 1) {
     return say;
   } else {
       let count = 0;
       let currentValue = ''
-      // let currentSay = say;
-      
+
+      // iterate n - 1 times to keep updating say
       for (let i = 0; i < n - 1; i ++) {
+        //initialize the new values every loop
         let newSay = '';
         currentValue = say[0];
+        // iterate through the current say to get the counts of each number
         for (let j = 0; j < say.length; j ++) {
           if (say[j] === currentValue) {
             count += 1;
+            // end the loop if we reach the last index
             if (j === say.length - 1) {
               newSay = newSay + count.toString() + currentValue.toString();
               count = 0;
             }
-          } else {
+          } 
+          // if the jth number isnt a match then we add the countandsay of the prior number
+          // and start a new loop starting at the non-matching index
+          else {
               newSay = newSay + count.toString() + currentValue.toString();
+              // set the new currentValue to the current value say[j]
+              currentValue = say[j]
+              // need to go back one step so j++ starts on the current index
               j = j -1;
-              if(say[j + 1]) {
-              currentValue = say[j + 1]
-              } 
               count = 0;
           }
-        }  
+        }
+        // update the say variable and start the next ith loop  
         say = newSay;
       }
   }
