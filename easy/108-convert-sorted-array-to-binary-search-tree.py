8a06acd8f96de3322dde6f7ecf0ba3bac3ecc26b
HypeDis-2 <30463842+HypeDis@users.noreply.github.com>
2018-11-02 01:44:20 -0700

    finished problem 108. had to look at discussions. need to get better at trees and recursion

---
+++ easy/108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,52 @@
+# Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+# For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+# Example:
+
+# Given the sorted array: [-10,-3,0,5,9],
+
+# One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+#       0
+#      / \
+#    -3   9
+#    /   /
+#  -10  5
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+def populateTree(nodeArray):
+    #  find mid point of the array
+    if len(nodeArray) == 0:
+        return None
+    midPoint = len(nodeArray) // 2
+    leftArray = nodeArray[:midPoint:]
+    rightArray = nodeArray[midPoint + 1::]
+    node = TreeNode(nodeArray[midPoint])
+    node.left = populateTree(leftArray)
+    node.right = populateTree(rightArray)
+    return node
+    # create a left and right array
+    # make midpoint that the current node
+    # recursively run the function with the left and right arrays node.left = and node.right =
+    #  return the node
+
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        return populateTree(nums)
+
+
+soln = Solution()
+print(soln.sortedArrayToBST([1, 2, 3, 4]).val)
