3a251e0890c7447b3b11ae837ac154a1292c4ed3
HypeDis-2 <30463842+HypeDis@users.noreply.github.com>
2018-11-05 23:01:38 -0800

    finished 110. had to look at discussion. still not quite sure how the recursion works

---
+++ easy/110-balanced-binary-tree.py
@@ -0,0 +1,53 @@
+# Given a binary tree, determine if it is height-balanced.
+
+# For this problem, a height-balanced binary tree is defined as:
+
+#     a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+# Example 1:
+
+# Given the following tree [3,9,20,null,null,15,7]:
+
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+
+# Return true.
+
+# Example 2:
+
+# Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+#        1
+#       / \
+#      2   2
+#     / \
+#    3   3
+#   / \
+#  4   4
+
+# Return false.
+#  for a given level in  the tree check for balance.
+# keep moving levels until you either reach the end or its unbalanced.
+# Definition for a binary tree node.
+
+
+def checkBalance(node):
+    if node == None:
+        return (0, True)
+    leftDepth, leftBalance = checkBalance(node.left)
+    rightDepth, rightBalance = checkBalance(node.right)
+    isNodeBalanced = abs(leftDepth - rightDepth) <= 1
+
+    return max(rightDepth, leftDepth) + 1, leftBalance and rightBalance and isNodeBalanced
+
+
+class Solution:
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return checkBalance(root)[1]
