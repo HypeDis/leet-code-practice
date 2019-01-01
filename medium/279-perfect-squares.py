1d8872d18a1b94ae0987bc251fdcc951e8197166
HypeDis-2 <30463842+HypeDis@users.noreply.github.com>
2018-12-31 16:46:18 -0800

    added problem description to file

--- medium/279-perfect-squares.py
+++ medium/279-perfect-squares.py
@@ -1,3 +1,17 @@
+""" Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9. """
+
 from collections import deque
 
 
@@ -58,4 +72,3 @@ class Solution:
 
 soln = Solution()
 print(soln.numSquares(48))
-# 7168
